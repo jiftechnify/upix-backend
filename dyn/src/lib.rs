dd43b5f3ae59215491683204ecc28f90db969b0f
jiftechnify <jiftech.stlfy@gmail.com>
2024-06-24 01:02:51 +0900

    prepare for dynamic upscaler

---
+++ dyn/src/lib.rs
@@ -0,0 +1,7 @@
+use worker::*;
+
+#[event(fetch)]
+async fn fetch(_req: Request, _env: Env, _ctx: Context) -> Result<Response> {
+    console_error_panic_hook::set_once();
+    Response::ok("Hello World!")
+}
